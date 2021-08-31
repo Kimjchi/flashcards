e6b13a9ef274629e7cb033cc2cd3b3486b3aa565
Jeremy Kim <jeremy@smartly.io>
2021-09-01 00:12:37 +0200

    Add quizzes reducer and connect with React

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addQuizId } from "../topics/topicsSlice";
+
+const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: {
+        quizzes: {}
+    },
+    reducers: {
+        addQuiz: (state, action) => {
+            const { id, name, topicId, cardIds } = action.payload;
+            state.quizzes[id] = {
+                id, name, topicId, cardIds
+            }
+        }
+    }
+});
+
+export const addQuizForTopicId = (quiz) => {
+    const { topicId, id } = quiz;
+    return (dispatch) => {
+      dispatch(quizzesSlice.actions.addQuiz(quiz));
+      dispatch(addQuizId({ topicId: topicId, quizId: id }));
+    };
+  };
+
+export const selectQuizzes = state => state.quizzes.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+
+export default quizzesSlice.reducer;
